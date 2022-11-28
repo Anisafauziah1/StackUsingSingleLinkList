2150a875028356022ff27c2cdc806c179f16e5df
Anisa Fauziah <anisa.fauziah.ft21@mail.umy.ac.id>
2022-11-28 16:32:47 +0700

    Add public void  push

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -31,6 +31,19 @@ namespace StackUsingSingleLinkList
             else
                 return (false);
         }
+        public void push(int element)
+        {
+            node fresh;
+            fresh = new node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + "pushed");
+        }
+        public void pop()
+        {
+
+        }
     }
     internal class Program
     {
